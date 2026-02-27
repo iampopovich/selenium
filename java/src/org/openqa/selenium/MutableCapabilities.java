8e66cb5d3a8aa9b4a436a81acb62465109747725
Alex Popov <iampopovich@gmail.com>
2026-02-28 01:19:40 +0300

    replace unmodifiable collections in MutableCapabilities.java

--- java/src/org/openqa/selenium/MutableCapabilities.java
+++ java/src/org/openqa/selenium/MutableCapabilities.java
@@ -30,12 +30,7 @@ public class MutableCapabilities implements Capabilities {
   private static final Set<String> OPTION_KEYS;
 
   static {
-    HashSet<String> keys = new HashSet<>();
-    keys.add("goog:chromeOptions");
-    keys.add("ms:edgeOptions");
-    keys.add("moz:firefoxOptions");
-    keys.add("se:ieOptions");
-    OPTION_KEYS = Collections.unmodifiableSet(keys);
+    OPTION_KEYS = Set.of("goog:chromeOptions", "ms:edgeOptions", "moz:firefoxOptions", "se:ieOptions");
   }
 
   private final Map<String, Object> caps = new TreeMap<>();
